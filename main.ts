c32e1c8153cee83d85caa21b9586264790036599
b-ananya <87336893+b-ananya@users.noreply.github.com>
2021-07-12 13:54:14 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,25 @@
-let mySprite: Sprite = sprites.create(img`
-    . . . . . . . . . . . . . . . .
-    . . . . c c c c c c c c . . . .
-    . . . c c b b b b b b c c . . .
-    . . c c b c c c c c c b c c . .
-    . c c b c c c c c c c c b c c .
-    . c b c f f 1 c c f f 1 c b c .
-    . c b c f f f c c f f f c b c .
-    . c b c f f f c c f f f c b c .
-    . c b c c c c c c c c c c b c .
-    . c b c c c c c c c c c c b c .
-    . c b c c f c c c c f c c b c .
-    . c c b c c f f f f c c b c c .
-    . . c c b c c c c c c b c c . .
-    . . . c c b b b b b b c c . . .
-    . . . . c c c c c c c c . . . .
-    . . . . . . . . . . . . . . . .
-`, SpriteKind.Player);
-
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestOpen, function (sprite, location) {
+    game.over(true)
+})
+tiles.setTilemap(tilemap`level1`)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . c c c c c c c c . . . . 
+    . . . c c b b b b b b c c . . . 
+    . . c c b c c c c c c b c c . . 
+    . c c b c c c c c c c c b c c . 
+    . c b c f f 1 c c f f 1 c b c . 
+    . c b c f f f c c f f f c b c . 
+    . c b c f f f c c f f f c b c . 
+    . c b c c c c c c c c c c b c . 
+    . c b c c c c c c c c c c b c . 
+    . c b c c f c c c c f c c b c . 
+    . c c b c c f f f f c c b c c . 
+    . . c c b c c c c c c b c c . . 
+    . . . c c b b b b b b c c . . . 
+    . . . . c c c c c c c c . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+tiles.placeOnRandomTile(mySprite, sprites.dungeon.collectibleInsignia)
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
---
+++ tilemap.g.ts
@@ -0,0 +1,47 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100002010103030303030301030103010303030301010101010301010301030103030103010301030303030303010301010101030303010101010101010103010301010301030103010301030103030303010103010301030303010101030101010101030103010301030303030301030303010301010103010101010101010301010103010303030301030103030103010101010103030101010301010101030101030301010101030103010303030303030103030303010301010101010101010101010101010103030103030303010303010303030303030101010101010101010103030303030301030303030303030101010101010101010301010101010104`, img`
+. . . 2 2 2 2 2 2 . 2 . 2 . 2 2 
+2 2 . . . . . 2 . . 2 . 2 . 2 2 
+. 2 . 2 . 2 2 2 2 2 2 . 2 . . . 
+. 2 2 2 . . . . . . . . 2 . 2 . 
+. 2 . 2 . 2 . 2 . 2 . 2 2 2 2 . 
+. 2 . 2 . 2 2 2 . . . 2 . . . . 
+. 2 . 2 . 2 . 2 2 2 2 2 . 2 2 2 
+. 2 . . . 2 . . . . . . . 2 . . 
+. 2 . 2 2 2 2 . 2 . 2 2 . 2 . . 
+. . . 2 2 . . . 2 . . . . 2 . . 
+2 2 . . . . 2 . 2 . 2 2 2 2 2 2 
+. 2 2 2 2 . 2 . . . . . . . . . 
+. . . . . . 2 2 . 2 2 2 2 . 2 2 
+. 2 2 2 2 2 2 . . . . . . . . . 
+. 2 2 2 2 2 2 . 2 2 2 2 2 2 2 . 
+. . . . . . . . 2 . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.floorLight2,sprites.dungeon.chestOpen,myTiles.tile1,sprites.dungeon.collectibleInsignia,myTiles.tile2], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
