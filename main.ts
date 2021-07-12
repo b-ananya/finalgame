bf287ee6920f81295f4bd3a196599c2aa4e47415
b-ananya <87336893+b-ananya@users.noreply.github.com>
2021-07-12 13:23:47 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
 let mySprite: Sprite = sprites.create(img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . .
+    . . . . c c c c c c c c . . . .
+    . . . c c b b b b b b c c . . .
+    . . c c b c c c c c c b c c . .
+    . c c b c c c c c c c c b c c .
+    . c b c f f 1 c c f f 1 c b c .
+    . c b c f f f c c f f f c b c .
+    . c b c f f f c c f f f c b c .
+    . c b c c c c c c c c c c b c .
+    . c b c c c c c c c c c c b c .
+    . c b c c f c c c c f c c b c .
+    . c c b c c f f f f c c b c c .
+    . . c c b c c c c c c b c c . .
+    . . . c c b b b b b b c c . . .
+    . . . . c c c c c c c c . . . .
+    . . . . . . . . . . . . . . . .
 `, SpriteKind.Player);
 
